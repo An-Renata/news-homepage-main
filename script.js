db55e7567a02c1306e6073ccbbbafaecb1315400
Renata <110370964+An-Renata@users.noreply.github.com>
2023-04-01 13:27:00 +0300

    update overlay while side menu is on

--- script.js
+++ script.js
@@ -2,10 +2,13 @@
 
 const menuButtonElement = document.querySelector(".hamburger-menu");
 const navigationLinks = document.querySelector(".nav-links");
+const overlayElement = document.querySelector(".overlay");
 
 let menuOpen = false;
 menuButtonElement.addEventListener("click", () => {
   navigationLinks.classList.toggle("active");
+  overlayElement.classList.toggle("faded");
+
   if (!menuOpen) {
     menuButtonElement.classList.add("open");
     menuOpen = true;
