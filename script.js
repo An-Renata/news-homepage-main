ddf04919cdd9439433e0a032b277819678997372
Renata <110370964+An-Renata@users.noreply.github.com>
2023-04-01 09:52:55 +0300

    updated media queries

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 
 const menuButtonElement = document.querySelector(".hamburger-menu");
 const navigationLinks = document.querySelector(".nav-links");
+
 let menuOpen = false;
 menuButtonElement.addEventListener("click", () => {
   navigationLinks.classList.toggle("active");
-
   if (!menuOpen) {
     menuButtonElement.classList.add("open");
     menuOpen = true;
